0abaf373329ca14f917f29879d1e1e41dab8be9f
Proyectos de Pycharm <git@github.com:Aguilar-Grecia/PycharmProjects.git>
2025-07-21 21:48:44 -0600

    ultimo commit

--- MENU.py
+++ MENU.py
@@ -24,4 +24,81 @@ def fibonacci(n):
     if n <= 1:
         return n
     else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
+        return fibonacci(n - 1)
+
+def contar_letra(palabra, letra, indice):
+    if indice < 0:
+        return 0
+    if palabra[indice] == letra:
+        return 1 + contar_letra(palabra, letra, indice - 1)
+    else:
+        return contar_letra(palabra, letra, indice -1)
+
+def cadena_texto(cadena, texto):
+    if texto < 0:
+        return 0
+    else:
+        return cadena[texto] + cadena_texto(cadena, texto - 1)
+
+def potencia(base, exponente):
+    if exponente ==0:
+        return 1
+    else:
+        return base * potencia(base, exponente - 1)
+
+while True:
+    menu()
+    opcion = input("Por favor, selecciona una de las opciones. ")
+    match opcion:
+        case"1":
+            try:
+                n = int(input("Ingrese un número entero positivo. "))
+                if n  < 0:
+                    print("Recuerde que tiene que ser un numero positivo.")
+                else:
+                    print(f"Factorial n {n} es {factorial(n)}")
+            except ValueError:
+                print("Entrada inválida.")
+        case "2":
+            try:
+                n = int(input("Ingrese un número entero que sea positivo: "))
+                if n <=0:
+                    print("Recuerde que el numero debe ser mayor a 0.")
+                else:
+                    print(f"Suma de los primeros {n} números naturales es {suma_n(n)} ")
+            except ValueError:
+                print("Error al ingresar, vuelve a intentarlo.")
+        case "3":
+            try:
+                n = int(input("Ingrese el el número, y que sea positivo. "))
+                if n < 0:
+                    print("El número debe ser mayor o igual a 0.")
+                else:
+                    print(f"Fibonacci({n}) ={fibonacci(n)}")
+            except ValueError:
+                print("Error al ingresar, vuelve a intentarlo.")
+        case "4":
+            palabra = input("Ingrese una palabra: ")
+            letra = input("Ingrese una letra: ")
+            if len(letra) !=1:
+                print("Solo ingresa una letra.")
+            else:
+                resultado = contar_letra(palabra, letra, len(palabra)-1)
+                print(f"La letra {letra} aparece {resultado} veces en la palabra {palabra}")
+        case "5":
+            cadena = input("Ingrese una cadena de texto: ")
+            invertida = cadena_texto(cadena, len (cadena)- 1)
+            print("cadena de texto:", invertida)
+        case"6":
+            try:
+                base = int(input("Ingrese la base, que sea entero: "))
+                exponente = int(input("Ingrese la exponente, que se debe ser entero: "))
+                if exponente < 0:
+                    print("El exponente tiene que ser positivo.")
+                else:
+                    print(f"{base}^{exponente} = {potencia(base, exponente)}")
+            except ValueError:
+                print("Error al ingresar, vuelve a intentarlo.")
+        case"7":
+            print("Gracias por estar adentro del programa. ¡Nos veremos pronto!")
+            break
