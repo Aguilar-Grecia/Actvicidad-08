bb0e931b79b9790ec508240eaeb1744a738a779d
Proyectos de Pycharm <git@github.com:Aguilar-Grecia/PycharmProjects.git>
2025-07-21 21:50:14 -0600

    ultimo commit

--- MENU.py
+++ MENU.py
@@ -3,7 +3,7 @@ def menu():
     print("1. Calcular el factorial de un número.")
     print("2. Suma de los primeros N números naturales.")
     print("3. Calcular el n-ésimo número de Fibonacci.")
-    print("4. Contar cuántas veces aparece una letra en una plabra.")
+    print("4. Contar cuántas veces aparece una letra en una palabra.")
     print("5. Invertir una cadena de texto.")
     print("6. Calcular la potencia de un número")
     print("7. Salir del programa.")
